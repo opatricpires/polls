bd3652d5a8c0f9ae3612a59646edf29d0b787496
opatricpires <patric.pires@outlook.com>
2024-02-08 12:28:09 -0300

    feat: option to vote on a poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,20 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
-app.register(createPoll, getPoll);
+
+app.register(cookie, {
+  secret: "polls-app-nlw",
+  hook: "onRequest",
+  parseOptions: {},
+});
+
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running...");
